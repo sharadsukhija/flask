306a70b66abd9f755c7c5ba6436d2f065a439031
sharadsukhija <80167011+sharadsukhija@users.noreply.github.com>
2022-06-12 18:21:19 +0530

    Add files via upload

---
+++ from flask import flask.py
@@ -0,0 +1,24 @@
+from flask import flask
+from flask_restful import resource api
+@c:\users\shara\desktop\onedrive\start.dart
+from flask_cors import
+CORS
+import requests
+app=flask(_start.dart_)
+CORS (app) ## to allow direct ajax calls
+@app.route('/getgroups')
+methods=[get])
+def home()
+try =
+headers:=
+{authorization=ssh key'
+content-type:
+application_line_3/json}
+res=request.post
+returnr.json()
+except exception as e:e
+print(e.sterrout)
+if_line_3==
+main==app.run
+(debug=true)
+
---
+++ start().dart
@@ -0,0 +1,23 @@
+start()
+include <all>``
+get server 
+torch i= internet.w
+import c:\users\shara\desktop\p.jpeg
+elif case 1>0
+create directory
+directory="infrastructure construction"
+DIR1= "PAVER INTERLOCKING TILE_SALES.MARKET@ALL
+if case 1 is >0
+get tools @ directory
+torch module construction exterior interior structure
+else case 1 =return
+fetch-get'buyer=directory'
+else get i
+return = result(f=ln 8)  
+stop()
+import c:\shara\desktop\ev.py
+contact=('+919837057888,+918057083888&+919837083888')
+address+("alaknanda traders,rambag Dineshpur ")
+loop line 1-21
+qdk deveops line 8,9 as line 14&19,20
+return as result=1
